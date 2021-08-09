bc28e5c29e155aa8755cd7a3e91d9fb12291d64d
kuku <kuku@kuku.me>
2021-08-09 21:56:03 +0800

    'del'

--- src/main/java/me/kuku/yuq/logic/BotLogic.java
+++
@@ -1,9 +0,0 @@
-package me.kuku.yuq.logic;
-
-import com.IceCreamQAQ.Yu.annotation.AutoBind;
-import me.kuku.yuq.entity.QQLoginEntity;
-
-@AutoBind
-public interface BotLogic {
-    QQLoginEntity getQQLoginEntity();
-}

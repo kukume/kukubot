f3f022103d39678895771e1196159a29ce1e0ca3
kukume <kuku@kuku.me>
2021-09-25 22:20:34 +0800

    'delete'

--- src/main/kotlin/me/kuku/yuq/exception/err.kt
+++
@@ -1,8 +0,0 @@
-package me.kuku.yuq.exception
-
-
-class VerifyFailedException(msg: String): RuntimeException(msg)
-
-class BaiduAiException(msg: String): RuntimeException(msg)
-
-class BaiduException(msg: String): RuntimeException(msg)

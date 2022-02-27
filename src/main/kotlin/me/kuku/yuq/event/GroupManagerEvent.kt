fc154d14199ded1303d26f0c94c30ea0d5acf240
kuku <kuku@kuku.me>
2022-02-27 21:01:16 +0800

    fix

--- src/main/kotlin/me/kuku/yuq/event/GroupManagerEvent.kt
+++ src/main/kotlin/me/kuku/yuq/event/GroupManagerEvent.kt
@@ -3,10 +3,14 @@ package me.kuku.yuq.event
 import com.IceCreamQAQ.Yu.annotation.Event
 import com.IceCreamQAQ.Yu.annotation.EventListener
 import com.icecreamqaq.yuq.event.*
+import com.icecreamqaq.yuq.message.Message.Companion.firstString
 import com.icecreamqaq.yuq.message.Message.Companion.toCodeString
 import com.icecreamqaq.yuq.message.Message.Companion.toMessageByRainCode
+import com.icecreamqaq.yuq.message.buildMessage
 import com.icecreamqaq.yuq.mif
 import me.kuku.utils.DateTimeFormatterUtils
+import me.kuku.utils.JobManager
+import me.kuku.utils.MyUtils
 import me.kuku.yuq.entity.*
 import me.kuku.yuq.transaction
 import java.util.concurrent.ConcurrentHashMap
@@ -22,6 +26,8 @@ class GroupManagerEvent @Inject constructor(
     private val lastMessage = ConcurrentHashMap<Long, String>()
     private val lastQq = ConcurrentHashMap<Long, Long>()
     private val lastRepeatMessage = ConcurrentHashMap<Long, String>()
+
+    private val verifyMap = ConcurrentHashMap<String, Boolean>()
 
     @Event
     fun inter(e: GroupMessageEvent) = transaction {
@@ -138,10 +144,38 @@ class GroupManagerEvent @Inject constructor(
     @Event
     fun verification(e: GroupMemberJoinEvent) {
         val group = e.group
+        val member = e.member
+        val qq = e.member.id
         val groupEntity = groupService.findByGroup(group.id) ?: return
         if (groupEntity.config.entryVerification == Status.ON) {
-
+            val captchaCode = MyUtils.randomStr(4)
+            val message = buildMessage {
+                at(qq)
+                line()
+                textLine("欢迎加入本群，请您发送验证码，如若在5分钟内未发送，将会被移除本群")
+                text("验证码为$captchaCode")
+            }
+            group.sendMessage(message)
+            val key = group.id.toString() + qq + captchaCode
+            verifyMap[key] = false
+            JobManager.delay(1000 * 60 * 5) {
+                val status = verifyMap[key] ?: return@delay
+                verifyMap.remove(key)
+                if (!status) {
+                    member.kick("超时未验证")
+                    group.sendMessage("${qq}超时未验证，已被移出本群")
+                }
+            }
         }
+    }
+
+    @Event
+    fun verify(e: GroupMessageEvent) {
+        val group = e.group.id
+        val qq = e.sender.id
+        val captchaCode = e.message.firstString()
+        val key = group.toString() + qq + captchaCode
+        verifyMap.remove(key)
     }
 }
 

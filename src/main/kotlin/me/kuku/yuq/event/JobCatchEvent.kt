df53b1830912d23ff72448561fce919d647808c7
kukume <kuku@kuku.me>
2022-05-27 16:11:20 +0800

    del unused import

--- src/main/kotlin/me/kuku/yuq/event/JobCatchEvent.kt
+++ src/main/kotlin/me/kuku/yuq/event/JobCatchEvent.kt
@@ -7,9 +7,6 @@ import com.IceCreamQAQ.Yu.annotation.EventListener
 import com.IceCreamQAQ.Yu.event.events.EventListenerRunExceptionEvent
 import com.IceCreamQAQ.Yu.event.events.JobRunExceptionEvent
 import me.kuku.utils.JobManager
-import me.kuku.utils.OkHttpUtils
-import me.kuku.yuq.entity.ExceptionLogEntity
-import me.kuku.yuq.entity.ExceptionLogService
 import me.kuku.yuq.entity.save
 import org.springframework.stereotype.Component
 
